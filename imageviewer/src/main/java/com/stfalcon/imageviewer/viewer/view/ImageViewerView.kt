38f81b21995e8aa0bfb8e8d688d005efa5d62d53
joy.chen <joy.chen@justalk.com>
2023-05-19 17:02:55 +0800

    Revert "bug: 拖动过程中触发了 onLongClick 或在拖动结束时触发了 onClick"
    This reverts commit 21c8f7e46d71704afdd03513ce532edd16f7181f.

--- imageviewer/src/main/java/com/stfalcon/imageviewer/viewer/view/ImageViewerView.kt
+++ imageviewer/src/main/java/com/stfalcon/imageviewer/viewer/view/ImageViewerView.kt
@@ -104,7 +104,6 @@ internal class ImageViewerView<T> @JvmOverloads constructor(
     private var wasDoubleTapped = false
     private var isOverlayWasClicked = false
     private var swipeDirection: SwipeDirection? = null
-    private var swipeDismissHandling = false
 
     private var transitionImageAnimator: TransitionImageAnimator? = null
     private var trackEnable = false
@@ -324,12 +323,6 @@ internal class ImageViewerView<T> @JvmOverloads constructor(
         return when (swipeDirection) {
             UP, DOWN -> {
                 if (isSwipeToDismissAllowed && !wasScaled && isIdle) {
-                    // 判定进入滑动退出状态时，触发一次 dispatchTouchEvent，避免 item View 因只接收到
-                    // ACTION_DOWN event，而在拖动过程中触发 onLongClick 或在拖动结束时触发 onClick
-                    if (!swipeDismissHandling) {
-                        swipeDismissHandling = true
-                        imagesPager.dispatchTouchEvent(event)
-                    }
                     swipeDismissHandler.onTouch(rootContainer, event)
                 } else true
             }
@@ -388,7 +381,6 @@ internal class ImageViewerView<T> @JvmOverloads constructor(
     private fun handleEventActionDown(event: MotionEvent) {
         swipeDirection = null
         wasScaled = false
-        swipeDismissHandling = false
         imagesPager.dispatchTouchEvent(event)
         swipeDismissHandler.onTouch(rootContainer, event)
         isOverlayWasClicked = dispatchOverlayTouch(event)
